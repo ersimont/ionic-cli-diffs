ac36b27232845a3e345e674aa9df5cea92576d04
ersimont <8042088+ersimont@users.noreply.github.com>
2021-06-22 19:49:01 -0400

    Initial commit

---
+++ generate-version.ts
@@ -0,0 +1,57 @@
+import * as rimraf from "rimraf";
+import { execSync, ExecSyncOptions } from "child_process";
+
+const projectName = "the-project";
+const applicationName = "the-application";
+const libraryName = "the-library";
+
+const version = process.argv[2];
+const flags = new Set(process.argv.slice(3));
+const branch = version + Array.from(flags).sort().join("");
+console.log("generating", branch);
+
+execSync(`git checkout -b ${branch}`);
+
+rimraf.sync(projectName);
+
+runAndCommit(`yarn add -D @angular/cli@${version}`);
+runAndCommit(
+  `npx ng new ${projectName} --createApplication=${!flags.has(
+    "-noApp"
+  )} --routing=${flags.has("-route")} --strict=${flags.has(
+    "-strict"
+  )} --skip-install --no-interactive --packageManager=yarn --style=scss`
+);
+if (flags.has("-subApp")) {
+  runAndCommit(
+    `npx ng generate application ${applicationName} --skip-install --no-interactive`,
+    { cwd: projectName }
+  );
+}
+if (flags.has("-lib")) {
+  runAndCommit(
+    `npx ng generate library ${libraryName} --skip-install --no-interactive`,
+    { cwd: projectName }
+  );
+}
+if (flags.has("-mat")) {
+  runAndCommit("npx ng add @angular/material --interactive=false --skip-confirmation=true", {
+    cwd: projectName,
+  });
+}
+if (flags.has("-pwa")) {
+  runAndCommit("npx ng add @angular/pwa --interactive=false --skip-confirmation=true", {
+    cwd: projectName,
+  });
+}
+if (flags.has("-fire")) {
+  console.error("@angular/fire must be added manually. Run this and commit:");
+  console.log("cd the-project && npx ng add @angular/fire");
+}
+rimraf.sync(`${projectName}/node_modules`);
+
+function runAndCommit(command: string, options?: ExecSyncOptions) {
+  execSync(command, options);
+  execSync("git add .");
+  execSync(`git commit -m "${command}"`);
+}
